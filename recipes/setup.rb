b97b8920db68864476da462dd5fb9836088d9f93
Fizbite <gherasima12@gmail.com>
2020-05-05 19:19:46 -0400

    modification

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -8,7 +8,26 @@ package 'ntp' do
 end
 
 file '/etc/motd' do
-  content 'Proprety of Alexandru Ghrasim'
+  content "Proprety of Alexandru Ghrasim
+  HOSTNAME: #{node['hostname']}
+  IPADDRESS: #{node['ipaddress']}
+  CPU: #{node['cpu']['0']['mhz']}
+  Memory: #{node['memory']['total']}
+"
   owner 'root'
   group 'root'
+  action :create
 end
+
+
+  
+
+file '/var/www/html/index.html' do
+  content "<h1> Fizbite </h1>
+  <h2>IPADDRESS: #{node['ipaddress'] } </h2>
+  <h2>HOSTNAME: #{node['hostname'] } </h2>
+"
+end
+
+
+
